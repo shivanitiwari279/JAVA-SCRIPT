83f4223c723fe9c2f2dd974d6ac0d28d13dcd54f
shivanitiwari359 <shivanitiwariwork@gmail.com>
2026-09-26 10:20:42 +0530

    Updating JS

---
+++ 01-basics/variables.js
@@ -0,0 +1,16 @@
+const accountID="1234"
+let accountPassword="2006"
+let accountName="Shivani"
+let accountCity="Jaipur"
+
+
+ accountPassword="4321"
+ accountName="Sam"
+ accountCity="Banglore"
+
+console.log(accountID);
+console.log(accountPassword);
+
+
+console.table([accountID,accountPassword,accountName,accountCity]);  //we use ".table" when we have to print evry variable without letting them  
+                                                                      //take again again with "consol.log()"
